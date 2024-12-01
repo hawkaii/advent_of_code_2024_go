1b8badd8f42650d4218bd8b89539da45be364719
hawkaii <parthibmukherjee@gmail.com>
2024-12-01 13:49:17 +0530

    - readme added

---
+++ Readme.m
@@ -0,0 +1,31 @@
+# Advent of Code 2024 - Go Solutions 🦾🎄
+
+Welcome to my repository for **Advent of Code 2024**! This is where I’ll be sharing my solutions, insights, and approaches to each day's coding challenges, all implemented in **Go (Golang)**.
+
+
+### Language: **Go**
+I chose Go for its simplicity, performance, and suitability for algorithmic challenges.
+
+### Features:
+- **Detailed Write-ups**: Every solution is accompanied by a detailed blog post in the `blogs/` directory.
+- **Reusable Functions**: Utility functions for tasks like file handling and data parsing are abstracted for reuse.
+
+---
+
+## 🔗 Private Leaderboard
+
+Compete with me and others in a friendly leaderboard competition! 🎉  
+Join using the following code:  
+**Leaderboard Code**: `3166487-0c0b97a7`
+
+
+---
+
+
+## 📬 Contact
+
+Have questions or suggestions? Feel free to reach out:
+- GitHub Issues: [Create an Issue](https://github.com/hawkaii/advent_of_code_2024_go/issues)
+
+Let’s make this Advent of Code a learning experience for everyone! Happy coding! 🎄✨
+
